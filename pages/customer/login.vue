dc1bd20e587287768b06ee7ce98f89bd36f7d553
dm-neslihan <genctuerk@digitalmanufaktur.com>
2020-11-18 23:54:04 +0100

    update template of login route w/ updated breakpoints, update assets

--- pages/customer/login.vue
+++ pages/customer/login.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="login-content">
         <b-row class="no-gutters">
-            <b-col cols="12" lg="4"  class="login-content__container--left vh-100">
+            <b-col cols="12" lg="4" class="login-content__container--left vh-100">
                 <div class="login-content__titles" :style="marginBottomTitle">
                     <h3>Sign in to Time Tracker</h3>
                     <h5>Welcome back</h5>
                 </div>
                 <b-row align-h="center" v-if="$mq === 'sm' || $mq === 'md'" :class="{ 'pt-5': $mq === 'sm', 'pt-1': $mq === 'md', 'pb-2': $mq === 'md' }">
-                    <img src="~/assets/images/illustration.svg" alt="illustration" :height="$mq === 'md' ? '250px' : '180px'">
+                    <img src="~/assets/images/login.png" alt="illustration" :height="$mq === 'md' ? '250px' : '180px'">
                 </b-row>
                 <form id="login" class="col-12 mt-4">
                     <div class="row">
@@ -30,7 +30,7 @@
                             @keyup.enter.prevent="setUser()"
                         />
                     </div>
-                    <div class="row login-content__problem d-flex mt-2" :class="{ 'justify-content-between': $mq === 'lg', 'flex-column align-items-center': $mq === 'sm' || $mq === 'md' }">
+                    <div class="row login-content__problem d-flex mt-2" :class="{ 'justify-content-between': $mq === 'lg' || $mq === 'plg', 'flex-column align-items-center': $mq === 'sm' || $mq === 'md' || $mq === 'mdp' }">
                         <div class="login-content__error-message" :class="{ 'ml-4': $mq === 'lg' }">{{ errorMessage }}</div>
                         <div :class="{ 'mr-3': $mq === 'lg' }"><a :href="directLinkPasswordReset">Forgot password?</a></div>
                     </div>
@@ -42,12 +42,12 @@
                     </div>
                 </form>
             </b-col>
-            <b-col cols="12" lg="8" class="login-content__container--right vh-100" v-if="$mq === 'lg'">
+            <b-col cols="12" lg="8" class="login-content__container--right vh-100" v-if="$mq === 'lg' || $mq === 'plg' || $mq === 'mdp'">
                 <div class="login-content__titles">
                     <h3>Time Tracker</h3>
                     <h5>by dmf</h5>
                 </div>
-                <img src="~/assets/images/illustration.svg" alt="illustration" :height="$mq === 'lg' ? '376px' : '275px'" class="mt-5">
+                <img src="~/assets/images/login.png" alt="illustration" :height="($mq === 'lg' || $mq === 'plg') ? '376px' : '275px'" class="mt-5">
             </b-col>
         </b-row>
     </div>
