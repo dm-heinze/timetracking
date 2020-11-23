d0663c3f6aaf3f12b2ed67920af5201f68db730e
dm-neslihan <genctuerk@digitalmanufaktur.com>
2020-11-23 22:04:56 +0100

    refactor axios calls in store: define baseURL as part of the URL, add baseURL as utility: val based on prod/dev mode

--- store/moduleUser.js
+++ store/moduleUser.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import _ from 'lodash';
 import base64 from 'base-64';
+import { __baseURL } from "../utility/constants";
 
 export const state = () => ({
     sessionObject: {}, // currently has 2 fields: name & value,
@@ -231,7 +232,7 @@ export const actions = {
     },
     requestSavingSingleWorklog: function ({ state, commit, dispatch }, payload) {
         return new Promise(async (resolve, reject) => {
-            axios.post('/api/addWorklog', { headers: getters.getHeader(state), comment: payload.comment, timeSpentSeconds: payload.timeSpentSeconds, ticketId: payload.ticketId })
+            axios.post(`${__baseURL}/api/addWorklog`, { headers: getters.getHeader(state), comment: payload.comment, timeSpentSeconds: payload.timeSpentSeconds, ticketId: payload.ticketId })
                 .then(() => {
                     commit('markTaskAsBooked', { taskToMarkAsBooked: payload.uniqueId });
 
@@ -252,7 +253,7 @@ export const actions = {
                 if (!hasNonTrackedTasks && !hasUnassignedCustomTasks) {
                     try {
                         await Promise.all(state.selectedTasks.map(async __selectedTask => {
-                            if (!__selectedTask.booked) await axios.post('/api/addWorklog', { headers: getters.getHeader(state), comment: __selectedTask.comment, timeSpentSeconds: __selectedTask.timeSpent, ticketId: __selectedTask.key })
+                            if (!__selectedTask.booked) await axios.post(`${__baseURL}/api/addWorklog`, { headers: getters.getHeader(state), comment: __selectedTask.comment, timeSpentSeconds: __selectedTask.timeSpent, ticketId: __selectedTask.key })
                         }))
                             .then(() => {
                                 state.selectedTasks.forEach((__bookedTask) => commit('markTaskAsBooked', { taskToMarkAsBooked: __bookedTask.uniqueId }));
@@ -312,7 +313,8 @@ export const actions = {
     },
     requestSessionRemoval: function({ commit, state, dispatch }) {
         return new Promise((resolve, reject) => {
-            axios.delete('/api/logout', { params: { value: state.sessionObject.value } })
+            // todo
+            axios.delete(`${__baseURL}/api/logout`, { params: { value: state.sessionObject.value } })
                 .then((_response) => {
                     if (_response.data.status === 204) {
                         dispatch('resetState')
@@ -328,8 +330,7 @@ export const actions = {
     },
     createApiObject: function({ commit, state, dispatch }, payload) {
         return new Promise((resolve, reject) => {
-
-            axios.post('/api/login', { username: payload.data.name, password: payload.data.pass })
+            axios.post(`${__baseURL}/api/login`, { username: payload.data.name, password: payload.data.pass })
                 .then(async (response) => {
                     if (response.data) { // todo
                         if (response.data === 401) reject("Your credentials are not valid."); // todo
@@ -361,8 +362,7 @@ export const actions = {
     },
     getIssue: function ({commit, state, dispatch}, payload) {
         return new Promise((resolve, reject) => {
-
-            axios.post('/api/getTickets', { headers: getters.getHeader(state), searchTerm: payload.searchTerm, currentUser: state.currentUser.name })
+            axios.post(`${__baseURL}/api/getTickets`, { headers: getters.getHeader(state), searchTerm: payload.searchTerm, currentUser: state.currentUser.name })
                 .then((__res) => {
                     if (__res.data.issues.length !== 0) {
                         const searchResults =__res.data.issues.map((__issueInSearchResult, index) => {
@@ -411,37 +411,19 @@ export const actions = {
         return new Promise((resolve, reject) => {
             // only re-fetch projects on initial load & reload/refresh
             if (state.allExistingProjects.length === 0) {
-                if (process.server) {
-                    axios({
-                        method: 'GET',
-                        url: process.env.BASE_DOMAIN + process.env.ENDPOINT_REST + 'project',
-                        headers: getters.getHeader(state),
+                axios.post(`${__baseURL}/api/getProjects`, { headers: getters.getHeader(state) })
+                    .then((__res) => {
+                        commit('setExistingProjects', __res.data);
+
+                        resolve();
                     })
-                        .then((__response) => {
-                            const stringifiedResponse = JSON.stringify(__response.data);
-
-                            commit('setExistingProjects', JSON.parse(stringifiedResponse));
-
-                            resolve();
-                        })
-                        .catch((err) => reject(err))
-                } else {
-                    axios.post('/api/getProjects', { headers: getters.getHeader(state) })
-                        .then((__res) => {
-                            commit('setExistingProjects', __res.data);
-
-                            resolve();
-                        })
-                        .catch((err) => reject(err))
-                }
-            } else {
-                resolve();
+                    .catch((err) => reject(err))
             }
         })
     },
     requestRelatedTickets: function ({commit, state, dispatch}, payload) {
         return new Promise((resolve, reject) => {
-            axios.post('/api/getProjectRelatedTickets', { headers: getters.getHeader(state), selectedProject: state.selectedProject })
+            axios.post(`${__baseURL}/api/getProjectRelatedTickets`, { headers: getters.getHeader(state), selectedProject: state.selectedProject })
                 .then((__res) => {
                     const __relatedTickets = __res.data.issues.map((__ticket) =>  {
                         return {
@@ -477,52 +459,17 @@ export const actions = {
     },
     requestSmartPickedIssues: function ({ state }) {
         return new Promise((resolve, reject) => {
-
-            if (process.server) {
-                axios({
-                    method: 'GET',
-                    url: process.env.BASE_DOMAIN + process.env.ENDPOINT_REST + 'issue/picker',
-                    headers: getters.getHeader(state),
-                })
-                    .then((__response) => {
-                        const stringifiedResponse = JSON.stringify(__response.data);
-
-                        resolve(JSON.parse(stringifiedResponse));
-                    })
-                    .catch((err) => reject(err))
-            } else {
-               axios.post('/api/getSmartPickedIssues', { headers: getters.getHeader(state) })
-                    .then((__res) => resolve(__res.data))
-                    .catch((err) => reject(err))
-            }
+            axios.post(`${__baseURL}/api/getSmartPickedIssues`, { headers: getters.getHeader(state) })
+                .then((__res) => resolve(__res.data))
+                .catch((err) => reject(err))
         })
     },
     requestAssignedTickets: function ({commit, state, dispatch}, payload) {
         return new Promise((resolve, reject) => {
-            if (process.server) {
-                const jqlSearchString = `assignee = currentUser() AND resolution = Unresolved order by updated DESC`;
+            axios.post(`${__baseURL}/api/getAssignedTickets`, { headers: getters.getHeader(state) })
+                .then((__res) => resolve(__res.data))
+                .catch((err) => reject(err))
 
-                axios({
-                    method: 'POST', // use POST to get the assigned tickets only
-                    url: process.env.BASE_DOMAIN + process.env.ENDPOINT_REST + 'search',
-                    data: { jql: jqlSearchString },
-                    headers: getters.getHeader(state),
-                })
-                    .then((__response) => {
-                        const stringifiedResponse = JSON.stringify(__response.data);
-
-                        resolve(JSON.parse(stringifiedResponse));
-                    })
-                    .catch((err) => {
-                        if (err.response.status === 401) console.log("no valid sessionId");
-
-                        reject(err);
-                    })
-            } else {
-                axios.post('/api/getAssignedTickets', { headers: getters.getHeader(state) })
-                    .then((__res) => resolve(__res.data))
-                    .catch((err) => reject(err))
-            }
         })
     },
     requestPrefill: function ({ commit, dispatch }) {
--- utility/constants.js
+++ utility/constants.js
@@ -1,1 +1,3 @@
 export const regexForTicketKeys = /[a-z]+\-[1-9]+[0-9]*$/i;
+
+export const __baseURL = (process.env.NODE_ENV !== 'production') ? 'http://localhost:3000' : process.env.VERCEL_URL;
