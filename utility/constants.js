d12e373cc1b5d55cdedc23e9bbf2122faed1acb3
dm-neslihan <genctuerk@digitalmanufaktur.com>
2020-11-23 22:12:12 +0100

    update constants file to reference VERCEL_ENV

--- utility/constants.js
+++ utility/constants.js
@@ -1,3 +1,3 @@
 export const regexForTicketKeys = /[a-z]+\-[1-9]+[0-9]*$/i;
 
-export const __baseURL = (process.env.NODE_ENV !== 'production') ? 'http://localhost:3000' : process.env.VERCEL_URL;
+export const __baseURL = (process.env.VERCEL_ENV !== 'preview') ? 'http://localhost:3000' : process.env.VERCEL_URL; // todo
