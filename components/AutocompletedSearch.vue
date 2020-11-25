7f7d0226f6b99014d56d416412f4b4b0e0545922
dm-neslihan <genctuerk@digitalmanufaktur.com>
2020-11-25 11:11:55 +0100

    refactor searchResults for AutocompleteSearch as separate component AutocompletedSearchResults - remove unused styles of transition fade - improve state update for alreadyExists

--- components/AutocompletedSearch.vue
+++ components/AutocompletedSearch.vue
@@ -20,22 +20,7 @@
         </div>
 
         <div v-if="!searchLoading && (searchTerm !== '')" class="autocompleted-search__results">
-            <b-list-group>
-                <b-list-group-item
-                    v-for="searchResult in searchResultList"
-                    :key="searchResult.key"
-                    class="d-flex justify-content-between"
-                    @click="addTicketToSelectedTasks(searchResult)"
-                >
-                    <div class="ticket__info col-11">
-                        <div class="ticket__info__key font-weight-bold">{{ searchResult.key }}</div>
-                        <div class="ticket__info__summary text-truncate">{{ searchResult.summary }}</div>
-                    </div>
-                    <div class="ticket__actions">
-                        <plus-circle-icon  class="ticket__icon align-self-center ticket__icon--selectable" />
-                    </div>
-                </b-list-group-item>
-            </b-list-group>
+            <autocompleted-search-results :search-term="searchTerm" :already-exists="alreadyExists" @updateAlreadyExists="onUpdateAlreadyExists" />
         </div>
         <div v-if="$nuxt.isOffline">No network connection available. You can track your time with a custom task instead.</div>
     </div>
@@ -44,15 +29,15 @@
 <script>
     import { mapMutations, mapState, mapActions } from 'vuex';
     import _ from "lodash";
-    import { SearchIcon, XIcon, PlusCircleIcon } from 'vue-feather-icons';
+    import { SearchIcon, XIcon } from 'vue-feather-icons';
+    import { BFormInput } from 'bootstrap-vue';
     import { regexForTicketKeys } from "~/utility/constants";
     import { searchAriaLabelMixin } from "~/utility/mixins";
-    import { BListGroupItem, BListGroup, BFormInput } from 'bootstrap-vue';
 
     export default {
         name: "AutocompletedSearch",
-        components: { SearchIcon, XIcon, PlusCircleIcon, BListGroupItem, BListGroup, BFormInput },
-        directives: { 'b-list-group': BListGroup, 'b-list-group-item': BListGroupItem, 'b-form-input': BFormInput },
+        components: { SearchIcon, XIcon, BFormInput, AutocompletedSearchResults: () => import('./AutocompletedSearchResults') },
+        directives: { 'b-form-input': BFormInput },
         mixins: [searchAriaLabelMixin],
         data() {
             return {
@@ -64,47 +49,22 @@
         computed: {
             ...mapState({
                 prefilledSearchSuggestions: state => state.moduleUser.prefilledSearchSuggestions,
-                searchResults: state => state.moduleUser.searchResults,
-                selectedTasks: state => state.moduleUser.selectedTasks,
                 isTimerActive: state => state.moduleUser.isTimerActive
-            }),
-            searchResultList () {
-                if (!this.alreadyExists) {
-                    let resultsToBeDisplayed = _.cloneDeep(this.searchResults);
-                    let keyOfResultsToBeDisplayed = resultsToBeDisplayed.map((__resultToBeDisplayed) => __resultToBeDisplayed.key);
-
-                    this.prefilledSearchSuggestions.forEach((__prefilledSuggestion) => {
-                        if (!_.includes(keyOfResultsToBeDisplayed, __prefilledSuggestion.key)) resultsToBeDisplayed.push(__prefilledSuggestion);
-                    })
-
-                    return resultsToBeDisplayed;
-                }
-
-                if (this.alreadyExists) {
-                    const __searchTerm = this.searchTerm;
-
-                    const __sorted = _.orderBy(this.prefilledSearchSuggestions, [function (__searchResult) {
-                        return _.isEqual(__searchResult.key, __searchTerm.toUpperCase());
-                    }])
-
-                    return _.reverse(__sorted);
-                }
-            }
+            })
         },
         methods: {
             ...mapMutations({
                 setSearchResult: 'moduleUser/setSearchResult', // todo
-                addSelectedTask: 'moduleUser/addSelectedTask',
                 setIsTimerActive: 'moduleUser/setIsTimerActive',
                 setActiveTicket: 'moduleUser/setActiveTicket'
             }),
             ...mapActions({
                 getIssue: 'moduleUser/getIssue',
-                saveSelectedTasksToStorage: 'moduleUser/saveSelectedTasksToStorage',
                 resetState: 'moduleUser/resetState'
             }),
             resetSearch: function () { // todo
                 this.searchTerm = '';
+                if (this.alreadyExists) this.onUpdateAlreadyExists(false);
                 this.setSearchResult([]);
             },
             requestSearch: _.debounce(function () {
@@ -133,32 +93,13 @@
                                 }
                             })
                     } else {
-                        this.alreadyExists = true;
+                        this.onUpdateAlreadyExists(true);
                     }
                 }
             }, 1100),
-            addTicketToSelectedTasks: function (selection) {
-                let __selection = _.cloneDeep(selection);
-                __selection.uniqueId = _.now();
-                this.addSelectedTask(__selection);
-                this.saveSelectedTasksToStorage();
-                // this.searchTerm = ''; // toggles visibility of search results
-                this.alreadyExists = false;
+            onUpdateAlreadyExists: function (exists) {
+                this.alreadyExists = exists;
             }
         }
     }
 </script>
-
-<style scoped>
-    .fade-enter-active, .fade-leave-active {
-        transition: all .3s ease;
-    }
-
-    .fade-enter, .fade-leave-to {
-        opacity: 0;
-    }
-
-    .fade-enter-to, .fade-leave {
-        opacity: 1;
-    }
-</style>
