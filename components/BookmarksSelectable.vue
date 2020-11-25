6267484e1c7c12bf8f94e7e9832a6f3538d07a56
dm-neslihan <genctuerk@digitalmanufaktur.com>
2020-11-25 16:57:04 +0100

    refactor bookmarks into separate component BookmarksSelectable, remove unused lines from script

---
+++ components/BookmarksSelectable.vue
@@ -0,0 +1,46 @@
+<template>
+	<div>
+        <h3 class="sidebar__title">Bookmarks</h3>
+        <client-only>
+            <div v-if="bookmarked.length !== 0">
+                <b-list-group> <!-- todo: dynamically import -->
+                    <b-list-group-item
+                        v-for="bookmarkedTicket in bookmarked"
+                        :key="bookmarkedTicket.key"
+                        class="d-flex justify-content-between"
+                        @click="addToSelectedIssues(bookmarkedTicket, false)"
+                    >
+                        <div class="ticket__info col-11">
+                            <div class="ticket__info__key font-weight-bold">{{ bookmarkedTicket.key }}</div>
+                            <div class="ticket__info__summary text-truncate">{{ bookmarkedTicket.summary }}</div>
+                        </div>
+                        <plus-circle-icon class="ticket__icon align-self-center" />
+                    </b-list-group-item>
+                </b-list-group>
+            </div>
+            <div v-else>No Bookmarks Saved.</div>
+        </client-only>
+    </div>
+</template>
+
+<script>
+    import { mapState } from 'vuex';
+    import { PlusCircleIcon } from "vue-feather-icons";
+    import { BListGroup, BListGroupItem } from "bootstrap-vue";
+
+    export default {
+		name: "BookmarksSelectable",
+        components: { PlusCircleIcon, BListGroupItem, BListGroup },
+        directives: { 'b-list-group': BListGroup, 'b-list-group-item': BListGroupItem },
+        computed: {
+            ...mapState({
+                bookmarked: state => state.moduleUser.bookmarked
+            })
+        },
+        methods: {
+            addToSelectedIssues: function (assignedTicket, fromSearchResults) { // todo
+                this.$emit('updateSelectedIssues', assignedTicket, false)
+            }
+        }
+	}
+</script>
--- components/SearchResults.vue
+++ components/SearchResults.vue
@@ -12,61 +12,36 @@
         <div v-else>No Search Results for This Query</div>
 
 
-
-        <h3 class="sidebar__title">Bookmarks</h3>
-        <client-only>
-            <div v-if="bookmarked.length !== 0">
-                <b-list-group>
-                    <b-list-group-item
-                        v-for="bookmarkedTicket in bookmarked"
-                        :key="bookmarkedTicket.key"
-                        class="d-flex justify-content-between"
-                        @click="addToSelectedIssues(bookmarkedTicket, false)"
-                    >
-                        <div class="ticket__info col-11">
-                            <div class="ticket__info__key font-weight-bold">{{ bookmarkedTicket.key }}</div>
-                            <div class="ticket__info__summary text-truncate">{{ bookmarkedTicket.summary }}</div>
-                        </div>
-                        <plus-circle-icon class="ticket__icon align-self-center" />
-                    </b-list-group-item>
-                </b-list-group>
-            </div>
-            <div v-else>No Bookmarks Saved.</div>
-        </client-only>
+        <bookmarks-selectable @updateSelectedIssues="addToSelectedIssues" />
     </div>
 </template>
 
 <script>
     import { mapState, mapMutations, mapActions } from 'vuex';
-    import { PlusCircleIcon, BookmarkIcon } from 'vue-feather-icons';
-    import { BListGroupItem, BListGroup } from 'bootstrap-vue';
     import _ from "lodash";
     import Suggestions from "~/components/Suggestions"; // todo: dynamically import if visibility configurable
     import AssignedTickets from "~/components/AssignedTickets"; // todo: dynamically import
+    import BookmarksSelectable from "~/components/BookmarksSelectable"; // todo
     import { smartPickedIssuesMixin, assignedTicketsMixin } from "~/utility/mixins";
 
     export default {
         name: "SearchResults",
-        components: { AssignedTickets, Suggestions, PlusCircleIcon, BookmarkIcon, BListGroupItem, BListGroup },
+        components: { AssignedTickets, Suggestions, BookmarksSelectable },
         mixins: [smartPickedIssuesMixin, assignedTicketsMixin], // todo: as store getters
-        directives: { 'b-list-group': BListGroup, 'b-list-group-item': BListGroupItem },
         computed: {
             ...mapState({
                 searchResults: state => state.moduleUser.searchResults,
                 selectedTasks: state => state.moduleUser.selectedTasks,
                 prefilledSearchSuggestions: state => state.moduleUser.prefilledSearchSuggestions,
-                currentUser: state => state.moduleUser.currentUser.name,
-                bookmarked: state => state.moduleUser.bookmarked
+                currentUser: state => state.moduleUser.currentUser.name
             })
         },
         methods: {
             ...mapMutations({
-                addSelectedTask: 'moduleUser/addSelectedTask',
-                updateBookmarks: 'moduleUser/updateBookmarks'
+                addSelectedTask: 'moduleUser/addSelectedTask'
             }),
             ...mapActions({
-                saveSelectedTasksToStorage: 'moduleUser/saveSelectedTasksToStorage',
-                saveBookmarksToStorage: 'moduleUser/saveBookmarksToStorage'
+                saveSelectedTasksToStorage: 'moduleUser/saveSelectedTasksToStorage'
             }),
             addToSelectedIssues: function (selectedTicket,  fromSearchResults = true) { // todo
                 let __selection;
@@ -92,11 +67,6 @@
                 this.addSelectedTask(__selection);
 
                 this.saveSelectedTasksToStorage();
-            },
-            toggleBookmarked: function (searchResultToBeToggled, summary = '') { // todo
-                this.updateBookmarks({ bookmark: searchResultToBeToggled, summary });
-
-                this.saveBookmarksToStorage();
             }
         }
     }
