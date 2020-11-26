78bc5e5ebcec6f939d27bc71eba0f4c1bd977bce
dm-neslihan <genctuerk@digitalmanufaktur.com>
2020-11-26 10:43:14 +0100

    refactor settingsSidebar - extract Bookmarks into separate component BookmarksEditable & dynamically import on demand

---
+++ components/BookmarksEditable.vue
@@ -0,0 +1,51 @@
+<template>
+    <b-list-group>
+        <b-list-group-item
+            v-for="bookmarkedTicket in bookmarked"
+            :key="bookmarkedTicket.key"
+            class="d-flex justify-content-between"
+        >
+            <div class="ticket__info col-10">
+                <div class="ticket__info__key font-weight-bold">{{ bookmarkedTicket.key }}</div>
+                <div class="ticket__info__summary text-truncate">{{ bookmarkedTicket.summary }}</div>
+            </div>
+            <div class="ticket__actions">
+                <bookmark-icon class="ticket__icon align-self-center ticket__icon--bookmarked" @click="toggleBookmarked(bookmarkedTicket.key)" />
+                <plus-circle-icon  class="ticket__icon align-self-center ticket__icon--selectable" @click="addToSelectedIssues(bookmarkedTicket, false)" />
+            </div>
+        </b-list-group-item>
+    </b-list-group>
+</template>
+
+<script>
+    import _ from "lodash"; // todo
+    import { mapState, mapActions, mapMutations } from 'vuex';
+    import { BookmarkIcon, PlusCircleIcon } from 'vue-feather-icons';
+    import { BListGroup, BListGroupItem } from 'bootstrap-vue';
+    import { addTaskMixin, toggleBookmarkedMixin } from "~/utility/mixins";
+
+	export default {
+		name: "BookmarksEditable",
+        components: {
+            BookmarkIcon, PlusCircleIcon,
+            BListGroup, BListGroupItem,
+        },
+        mixins: [addTaskMixin, toggleBookmarkedMixin], // todo: move to comp or to store
+        directives: { 'b-list-group': BListGroup, 'b-list-group-item': BListGroupItem },
+        computed: {
+            ...mapState({
+                bookmarked: state => state.moduleUser.bookmarked
+            })
+        },
+        methods: {
+            ...mapActions({
+                saveBookmarksToStorage: 'moduleUser/saveBookmarksToStorage',
+                saveSelectedTasksToStorage: 'moduleUser/saveSelectedTasksToStorage'
+            }),
+            ...mapMutations({
+                updateBookmarks: 'moduleUser/updateBookmarks',
+                addSelectedTask: 'moduleUser/addSelectedTask'
+            })
+        }
+	}
+</script>
--- components/Settings.vue
+++ components/Settings.vue
@@ -8,23 +8,8 @@
         <search />
 
         <h3 class="sidebar__title sidebar__title--bookmarks">Bookmarks</h3>
-        <div v-if="bookmarked.length !== 0"> <!-- todo: dynamically import template block if true -->
-            <b-list-group>
-                <b-list-group-item
-                    v-for="bookmarkedTicket in bookmarked"
-                    :key="bookmarkedTicket.key"
-                    class="d-flex justify-content-between"
-                >
-                    <div class="ticket__info col-10">
-                        <div class="ticket__info__key font-weight-bold">{{ bookmarkedTicket.key }}</div>
-                        <div class="ticket__info__summary text-truncate">{{ bookmarkedTicket.summary }}</div>
-                    </div>
-                    <div class="ticket__actions">
-                        <bookmark-icon class="ticket__icon align-self-center ticket__icon--bookmarked" @click="toggleBookmarked(bookmarkedTicket.key)" />
-                        <plus-circle-icon  class="ticket__icon align-self-center ticket__icon--selectable" @click="addToSelectedIssues(bookmarkedTicket, false)" />
-                    </div>
-                </b-list-group-item>
-            </b-list-group>
+        <div v-if="bookmarked.length !== 0">
+            <bookmarks-editable />
         </div>
         <div v-else class="sidebar__title sidebar__title--no-bookmarks">No Bookmarks Saved.</div>
 
@@ -33,39 +18,30 @@
 </template>
 
 <script>
-    import { mapState, mapActions, mapMutations } from 'vuex';
-    import { XCircleIcon, BookmarkIcon, PlusCircleIcon } from 'vue-feather-icons';
-    import { BListGroup, BListGroupItem } from 'bootstrap-vue';
-    import _ from "lodash"; // todo
+    import { mapState, mapMutations } from 'vuex';
+    import { XCircleIcon } from 'vue-feather-icons';
     import Logout from '~/components/Logout';
     import Search from "~/components/Search";
-    import { addTaskMixin, toggleBookmarkedMixin, resetSearchMixin } from "~/utility/mixins";
+    import { resetSearchMixin } from "~/utility/mixins";
 
 	export default {
 		name: "Settings",
         components: {
+            XCircleIcon,
             Search,
-		    XCircleIcon, BookmarkIcon, PlusCircleIcon,
-            BListGroup, BListGroupItem,
+            BookmarksEditable: () => import('~/components/BookmarksEditable'),
             Logout
         },
-        directives: { 'b-list-group': BListGroup, 'b-list-group-item': BListGroupItem },
-        mixins: [addTaskMixin, toggleBookmarkedMixin, resetSearchMixin],
+        mixins: [resetSearchMixin],
         computed: {
             ...mapState({
                 bookmarked: state => state.moduleUser.bookmarked
             })
         },
         methods: {
-            ...mapActions({
-                saveBookmarksToStorage: 'moduleUser/saveBookmarksToStorage',
-                saveSelectedTasksToStorage: 'moduleUser/saveSelectedTasksToStorage'
-            }),
             ...mapMutations({
-                updateBookmarks: 'moduleUser/updateBookmarks',
                 setSearchResult: 'moduleUser/setSearchResult',
                 toggleSettings: 'moduleUser/toggleSettings',
-                addSelectedTask: 'moduleUser/addSelectedTask',
                 setSearchTerm: 'moduleUser/setSearchTerm'
             })
         }
