448d258e6ab6767627aeaac544af9b74f605a249
dm-neslihan <genctuerk@digitalmanufaktur.com>
2020-12-03 18:44:34 +0100

    refactor pages/index.vue: extract Break into separate component #inProgress - flexDirection computed property needed both in new Break component & pages/index.vue therefore now a mixin

--- pages/index.vue
+++ pages/index.vue
@@ -29,15 +29,7 @@
                             <div class="d-flex" :class="[flexDirection,  { 'pr-3': $mq === 'md' ||  $mq === 'lg' }]">
                                 <add-custom-task />
 
-                                <b-button pill @click.prevent="toggleBreak" v-b-toggle.breakTracker type="button" class="login-content__sign-in-btn py-2 mr-1" v-b-tooltip.hover title="Take a break" :class="{ 'mb-3': $mq === 'sm', 'px-5': $mq === 'md' || $mq === 'mdp' || $mq === 'plg' }">
-                                    <coffee-icon />
-                                    <span class="pl-1" v-if="$mq === 'lg' || $mq === 'sm'">Take a break</span>
-                                </b-button>
-                                <client-only>
-                                    <button  @click.prevent="resetBreakTracker" class="button--resetBreakTracker ml-1 mr-2 px-3" v-b-tooltip.hover title="Reset Break Tracker" ref="resetBreakButton" id="resetTrackedBreakTime" v-if="!onABreak && (accumulatedBreakTime != '00:00:00')">
-                                        <rotate-ccw-icon />
-                                    </button>
-                                </client-only>
+                                <break />
                             </div>
                             <div class="d-flex" :class="[flexDirection, { 'align-items-center': $mq === 'md' || $mq === 'lg' || $mq === 'mdp' || $mq === 'plg' }]">
                                 <span v-if="totalTime" :class="{ 'mr-3': $mq === 'md' || $mq === 'lg' || $mq === 'mdp' || $mq === 'plg', 'align-self-center': $mq === 'sm' }">worked so far: <span class="font-weight-bold">{{ totalTime }}</span></span> <!-- todo: condition -->
@@ -56,41 +48,31 @@
 <script>
     import _ from "lodash";
     import { mapState, mapActions, mapMutations } from 'vuex';
-    import { CoffeeIcon, PauseCircleIcon, RotateCcwIcon } from 'vue-feather-icons';
+    import { CoffeeIcon, PauseCircleIcon } from 'vue-feather-icons';
     import { BCollapse, BNavbarNav } from "bootstrap-vue";
     import SelectedTasks from "~/components/main/tasks/SelectedTasks";
     import AddCustomTask from "~/components/main/tasks/AddCustomTask";
     import PushTotalTime from "~/components/main/tasks/PushTotalTime";
     import SearchSidebar from "~/components/search-sidebar/SearchSidebar";
+    import Break from "~/components/main/Break";
+    import { mainButtonsFlexDirectionMixin } from "~/utility/mixins";
 
     export default {
         name: 'Index',
         components: {
+            Break,
             SearchSidebar,
             PushTotalTime,
             AddCustomTask, SelectedTasks, Settings: () => import('~/components/settings-sidebar/Settings'),
             BCollapse, BNavbarNav,
-            CoffeeIcon, PauseCircleIcon, RotateCcwIcon
+            CoffeeIcon, PauseCircleIcon
         },
+        mixins: [mainButtonsFlexDirectionMixin],
         directives: { 'b-collapse': BCollapse, 'b-navbar-nav': BNavbarNav },
-        data () {
-            return {
-                // break
-                timeRightNow: 0,
-                runningTimer: '',
-                startTime: '',
-                endTime: '',
-                initialLoggedBreak: ''
-            }
-        },
         computed: {
             ...mapState({
-                selectedTasks: state => state.moduleUser.selectedTasks,
-                // break
-                accumulatedBreakTime: state => state.moduleUser.accumulatedBreakTime,
-                onABreak: state => state.moduleUser.onABreak,
-                isTimerActive: state => state.moduleUser.isTimerActive,
-                activeTicket: state => state.moduleUser.activeTicket,
+                selectedTasks: state => state.moduleUser.selectedTasks, // totalTime
+                accumulatedBreakTime: state => state.moduleUser.accumulatedBreakTime, // break
                 settingsOpen: state => state.moduleUser.settingsOpen,
                 searchResults: state => state.moduleUser.searchResults
             }),
@@ -114,119 +96,30 @@
                 const dateFromTotalWorkTime = new Date(0, 0, 0, 0, 0,0, sumOfWorkedTime);  // todo
 
                 return dateFromTotalWorkTime.toTimeString().slice(0, 8); // remove any non-time related information
-            },
-            flexDirection () {
-                return `flex-${this.$mq === 'sm' ? 'column' : 'row'}`
             }
         },
         watch: {
-            onABreak: function(newValue) {
-                if (newValue) {
-                    this.initialLoggedBreak = this.accumulatedBreakTime;
-
-                    this.runningTimer = setInterval(() => this.currentTimeInSeconds(), 1000);
-                } else {
-                    // stop interval when onABreak is set to false
-                    this.endTime = new Date();
-
-                    clearInterval(this.runningTimer);
-
-                    // save to localStorage
-                    this.saveBreaksToStorage();
-                }
-            },
             settingsOpen: function(newValue) {
                 if (newValue && this.searchResults.length !== 0) this.setSearchResult([]);
             },
             $mq: function (newValue) {
                 if (newValue === 'lg' || newValue === 'sm') this.$root.$emit('bv::disable::tooltip');
                 else this.$root.$emit('bv::enable::tooltip');
-
-                // tooltip for resetBreakButton should always be visible on hover bc no other visible text available for it
-                if (this.$refs.resetBreakButton) this.$root.$emit('bv::enable::tooltip', 'resetTrackedBreakTime'); // todo
             }
         },
         methods: {
             ...mapActions({
-                requestAllProjects: 'moduleUser/requestAllProjects',
-                saveBreaksToStorage: 'moduleUser/saveBreaksToStorage',
+                requestAllProjects: 'moduleUser/requestAllProjects'
             }),
             ...mapMutations({
-                // break
-                toggleBreakMutation: 'moduleUser/toggleBreak',
-                updateTotalBreakTime: 'moduleUser/updateTotalBreakTime',
-                setIsTimerActive: 'moduleUser/setIsTimerActive',
-                setLastTicket: 'moduleUser/setLastTicket',
-                addBreak: 'moduleUser/addBreak',
+                toggleBreakMutation: 'moduleUser/toggleBreak', // break
                 setSearchResult: 'moduleUser/setSearchResult'
             }),
             // break
             toggleBreak: function () {
-                // todo
                 // start a break immediately
                 // -> step 1) set onABreak to true
-
                 this.toggleBreakMutation();
-
-                if (this.isTimerActive) {
-                    this.setLastTicket(this.activeTicket);
-
-                    // this stops any active timer bc of watcher on isTimerActive in SelectedTask
-                    this.setIsTimerActive();
-                }
-
-                this.startTime = new Date();
-            },
-            resetBreakTracker: function () {
-                // update vuex store
-                this.updateTotalBreakTime({ totalBreakTime: '00:00:00' });
-
-                // update localStorage & show feedback
-                this.saveBreaksToStorage()
-                    .then(() => {
-                        this.$bvModal.msgBoxOk('Break Tracker was successfully reset to 00:00:00.', {
-                            centered: true,
-                            okVariant: 'success rounded-pill',
-                            okTitle: 'Okay',
-                            bodyClass: 'modal__main-container',
-                            footerClass: 'modal__main-container modal__actions modal__feedback__footer'
-                        })
-                    })
-                    .catch(() => {
-                        this.$bvModal.msgBoxOk('There has been an error. Break Tracker could not be reset.', {
-                            centered: true,
-                            okVariant: 'danger rounded-pill',
-                            okTitle: 'Okay',
-                            bodyClass: 'modal__main-container',
-                            footerClass: 'modal__main-container modal__actions modal__feedback__footer'
-                        })
-                    });
-            },
-            currentTimeInSeconds: function () {
-                const __dateRightNow = new Date();
-
-                this.timeRightNow = __dateRightNow;
-
-                const calculatedDifference = this.timeRightNow.getTime() - this.startTime.getTime();
-
-                let breakTimeAsDate = new Date(__dateRightNow.getFullYear(), __dateRightNow.getMonth(), __dateRightNow.getDate(), 0, 0, 0, calculatedDifference);
-
-                // add previous tracked break time into calculation
-                if (this.accumulatedBreakTime != '00:00:00') {
-                    const initialBreakInArrayFormat = this.initialLoggedBreak.split(":");
-
-                    const currentBreakTimeInArrayFormat = breakTimeAsDate.toTimeString().slice(0, 8).split(":");
-
-                    const summedUpInitialAndNewBreak = _.zipWith(initialBreakInArrayFormat, currentBreakTimeInArrayFormat, (a, b) => Number(a) + Number(b)); // values need to be numbers not strings for correct calculation
-
-                    breakTimeAsDate = new Date(__dateRightNow.getFullYear(), __dateRightNow.getMonth(), __dateRightNow.getDate(), summedUpInitialAndNewBreak[0], summedUpInitialAndNewBreak[1], summedUpInitialAndNewBreak[2], 0);
-                }
-
-                // update vuex store with totalBreakTime
-                this.updateTotalBreakTime({ totalBreakTime:  breakTimeAsDate.toTimeString().slice(0, 8) });
-
-                // save to localStorage
-                this.saveBreaksToStorage();
             }
         },
         middleware ({ store, redirect }) {
@@ -246,9 +139,6 @@
             this.$nextTick(function () {
                 // do not show any tooltip for these screens as text visible & not icon-only
                 if (this.$mq === 'lg' || this.$mq === 'sm') this.$root.$emit('bv::disable::tooltip');
-
-                // show tooltip on all viewports as always icon-only
-                if (this.$refs.resetBreakButton) this.$root.$emit('bv::enable::tooltip', 'resetTrackedBreakTime'); // todo
             })
         }
     }
