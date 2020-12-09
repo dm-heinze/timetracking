0b8674afffc0adedda2b8cac299648c854bff5fd
dm-neslihan <genctuerk@digitalmanufaktur.com>
2020-12-09 22:26:46 +0100

    remove baseUrl when !process.server

--- utility/constants.js
+++ utility/constants.js
@@ -1,3 +1,4 @@
 export const regexForTicketKeys = /[a-z]+\-[1-9]+[0-9]*$/i;
 
-export const __base_url = process.server ? 'http://localhost:3000' : process.env.baseUrl;
+//export const __base_url = process.server ? 'http://localhost:3000' : process.env.baseUrl;
+export const __base_url = process.server ? 'http://localhost:3000' : '';
