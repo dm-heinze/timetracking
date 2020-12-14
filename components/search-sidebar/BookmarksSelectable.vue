110a7d896830fd14372fe00742a5fdfcc2c2b52b
dm-neslihan <genctuerk@digitalmanufaktur.com>
2020-12-14 21:09:01 +0100

    [FEATURE] - add amount toggling to bookmarks on searchSidebar if more than 5 bookmarks available

--- components/search-sidebar/BookmarksSelectable.vue
+++ components/search-sidebar/BookmarksSelectable.vue
@@ -1,11 +1,16 @@
 <template>
 	<div>
-        <h3 class="sidebar__title title--bookmarks">Bookmarks</h3>
+        <div class="d-flex pr-1 amountToggler amountToggler--suggestions" :class="{ 'no-toggle': this.bookmarked.length < 6 }">
+            <h3 class="sidebar__title title--bookmarks" @click="toggleShownAmount" v-b-hover="handleShowTogglerOnHover">Bookmarks</h3>
+            
+            <chevrons-down-icon v-if="!showMore && showTogglerIcon && this.bookmarked.length > 5" class="mx-2" />
+            <chevrons-up-icon v-else-if="showMore && showTogglerIcon && this.bookmarked.length > 5" class="mx-2" />
+        </div>
         <client-only>
             <div v-if="bookmarked.length !== 0">
                 <b-list-group> <!-- todo: dynamically import -->
                     <b-list-group-item
-                        v-for="bookmarkedTicket in bookmarked"
+                        v-for="bookmarkedTicket in bookmarkedToBeVisible"
                         :key="bookmarkedTicket.key"
                         class="d-flex justify-content-between"
                         @click="addToSelectedIssues({ selectedTicket: bookmarkedTicket, fromSearchResults: false })"
@@ -25,22 +30,41 @@
 
 <script>
     import { mapState, mapActions } from 'vuex';
-    import { PlusCircleIcon } from "vue-feather-icons";
-    import { BListGroup, BListGroupItem } from "bootstrap-vue";
+    import { ChevronsDownIcon, ChevronsUpIcon, PlusCircleIcon } from "vue-feather-icons";
+    import { BListGroup, BListGroupItem, VBHover } from "bootstrap-vue";
+    import _ from "lodash";
 
     export default {
 		name: "BookmarksSelectable",
-        components: { PlusCircleIcon, BListGroupItem, BListGroup },
-        directives: { 'b-list-group': BListGroup, 'b-list-group-item': BListGroupItem },
+        components: {
+		    PlusCircleIcon, ChevronsDownIcon, ChevronsUpIcon,
+            BListGroupItem, BListGroup },
+        directives: { 'b-list-group': BListGroup, 'b-list-group-item': BListGroupItem, 'b-hover': VBHover },
+        data() {
+            return {
+                showMore: true,
+                showTogglerIcon: false
+            }
+        },
         computed: {
             ...mapState({
                 bookmarked: state => state.moduleUser.bookmarked
-            })
+            }),
+            bookmarkedToBeVisible () {
+                if ((this.bookmarked.length < 6) || this.showMore) return this.bookmarked;
+                else return _.slice(this.bookmarked, 0 , 5); // end excluded
+            }
         },
         methods: {
             ...mapActions({
                 addToSelectedIssues: 'moduleUser/addToSelectedIssues'
-            })
-        }
+            }),
+            toggleShownAmount () {
+                this.showMore = !this.showMore;
+            },
+            handleShowTogglerOnHover () {
+                if (this.bookmarked.length > 5) this.showTogglerIcon = !this.showTogglerIcon;
+            }
+        },
 	}
 </script>
