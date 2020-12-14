6bec8da635785cec7c450bcf5857784033757f13
dm-neslihan <genctuerk@digitalmanufaktur.com>
2020-12-14 20:10:27 +0100

    [FEATURE] - add toggling to show more/less assignedTickets if more than 5 available

--- components/search-sidebar/AssignedTickets.vue
+++ components/search-sidebar/AssignedTickets.vue
@@ -1,7 +1,13 @@
 <template>
     <div>
         <div class="d-flex justify-content-between">
-            <h3 class="sidebar__title">Assigned Tickets</h3>
+            <div class="d-flex flex-row amountToggler" :class="{ 'no-toggle': this.assignedTickets.length < 5 }">
+                <h3 class="sidebar__title pr-1" @click="toggleShownAmount" v-b-hover="handleShowTogglerOnHover">Assigned Tickets</h3>
+
+                <chevrons-down-icon v-if="!showMore && showTogglerIcon && this.assignedTickets.length > 5" class="mx-2" />
+                <chevrons-up-icon v-else-if="showMore && showTogglerIcon && this.assignedTickets.length > 5" class="mx-2" />
+            </div>
+
             <button
                 id="refreshTooltipTargetButton"
                 ref="refreshTooltipTarget"
@@ -15,7 +21,7 @@
         </div>
         <b-list-group>
             <b-list-group-item
-                v-for="assignedTicket in assignedTickets"
+                v-for="assignedTicket in assignedTicketsToBeVisible"
                 :key="assignedTicket.uniqueId"
                 @click="addToSelectedIssues({ selectedTicket: assignedTicket, fromSearchResults: true })"
                 class="d-flex justify-content-between"
@@ -33,16 +39,23 @@
 
 <script>
     import { mapState, mapGetters, mapActions } from 'vuex';
-	import { PlusCircleIcon, DownloadCloudIcon } from "vue-feather-icons";
-    import { BListGroup, BListGroupItem } from "bootstrap-vue";
+    import { PlusCircleIcon, DownloadCloudIcon, ChevronsDownIcon, ChevronsUpIcon } from "vue-feather-icons";
+    import { BListGroup, BListGroupItem, VBHover } from "bootstrap-vue";
+    import _ from "lodash";
 
     export default {
 		name: "AssignedTickets",
         components: {
-		    PlusCircleIcon, DownloadCloudIcon,
+		    PlusCircleIcon, DownloadCloudIcon, ChevronsDownIcon, ChevronsUpIcon,
             BListGroupItem, BListGroup
         },
-        directives: { 'b-list-group': BListGroup, 'b-list-group-item': BListGroupItem },
+        directives: { 'b-list-group': BListGroup, 'b-list-group-item': BListGroupItem, 'b-hover': VBHover },
+        data() {
+            return {
+                showMore: true,
+                showTogglerIcon: false
+            }
+        },
         computed: {
 		    ...mapState({
                 assignedTickets: state => state.moduleUser.assignedTickets
@@ -51,13 +64,23 @@
                 // todo: flagged for possible deprecation
                 // filtered from aggregated search & request results
                 getAssignedTickets: 'moduleUser/getAssignedTickets'
-            })
+            }),
+            assignedTicketsToBeVisible () {
+                if ((this.assignedTickets.length < 6) || this.showMore) return this.assignedTickets;
+                else return _.slice(this.assignedTickets, 0 , 5); // end excluded
+            }
         },
         methods: {
 		    ...mapActions({
                 addToSelectedIssues: 'moduleUser/addToSelectedIssues',
                 refreshAssignedTickets: 'moduleUser/refreshAssignedTickets'
-            })
+            }),
+            toggleShownAmount () {
+                this.showMore = !this.showMore;
+            },
+            handleShowTogglerOnHover () {
+		        if (this.assignedTickets.length > 5) this.showTogglerIcon = !this.showTogglerIcon;
+            }
         },
         mounted() {
             // wait for rendering
