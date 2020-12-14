eb79d63d1117b3149aadfb36ceb689147c2e540b
dm-neslihan <genctuerk@digitalmanufaktur.com>
2020-12-14 16:32:35 +0100

    add modal for confirming resetting tracked break time or optionally cancelling reason: to prevent unintentional resets

--- components/main/tasks/BreakTask.vue
+++ components/main/tasks/BreakTask.vue
@@ -12,9 +12,39 @@
                     <ticket-time-spent :of-type-break="true" :time-spent="accumulatedBreakTimeInMilliSeconds" unique-id="breakTask" :booked="false" />
 
                     <client-only>
-                        <button @click.prevent="resetBreakTracker" class="btn--resetBreak pb-1" v-b-tooltip.hover title="Reset Break Tracker" ref="resetBreakButton" id="resetTrackedBreakTime" v-if="!onABreak && (accumulatedBreakTime != '00:00:00')">
+                        <button
+                            v-b-modal="`confirm-reset-break-modal`"
+                            class="btn--resetBreak pb-1"
+                            v-b-tooltip.hover
+                            title="Reset Break Tracker"
+                            ref="resetBreakButton"
+                            id="resetTrackedBreakTime"
+                            v-if="!onABreak && (accumulatedBreakTime != '00:00:00')"
+                        >
                             <rotate-ccw-icon />
                         </button>
+
+                        <b-modal :id="`confirm-reset-break-modal`" centered>
+                            <template v-slot:modal-header="{ close }">
+                                <div class="d-flex justify-content-between align-items-center w-100 modal__top-bar">
+                                    <h3 class="primary">Reset Tracked Break Time?</h3>
+                                    <span>
+                                        <x-icon @click="close()" />
+                                    </span>
+                                </div>
+                            </template>
+                            <template v-slot:default>
+                                <div class="modal__main-container">
+                                    <div class="modal__main-container__main-text">Do you want to delete the tracked break time of <span>{{ accumulatedBreakTime }}</span>?</div>
+                                </div>
+                            </template>
+                            <template v-slot:modal-footer="{ ok, cancel }">
+                                <div class="d-flex justify-content-between w-100 modal__actions">
+                                    <b-button pill class="font-weight-bold modal__cancel-btn" @click.prevent="cancel()">Cancel</b-button>
+                                    <b-button pill variant="primary" class="font-weight-bold modal__save-btn" @click.prevent="resetBreakTracker">Delete</b-button>
+                                </div>
+                            </template>
+                        </b-modal>
                     </client-only>
                 </div>
             </div>
@@ -25,14 +55,15 @@
 <script>
     import { mapActions, mapMutations, mapState } from 'vuex';
     import TicketTimeSpent from "~/components/main/tasks/task/TicketTimeSpent";
-    import { RotateCcwIcon } from 'vue-feather-icons';
+    import { RotateCcwIcon, XIcon } from 'vue-feather-icons';
     import _ from "lodash";
 
 
     export default {
         name: "BreakTask",
         components: {
-            TicketTimeSpent, RotateCcwIcon
+            TicketTimeSpent,
+            RotateCcwIcon, XIcon
         },
         computed: {
             ...mapState({
