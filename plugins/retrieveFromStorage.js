cb930f5265b43cf3eb55096d483d725fb798d506
dm-neslihan <genctuerk@digitalmanufaktur.com>
2020-12-23 11:06:25 +0100

    [QUICKFIX] - move setting/retrieving current date to client side as server side in nuxtServerInit will compare/set w/ a UTC based val

--- plugins/retrieveFromStorage.js
+++ plugins/retrieveFromStorage.js
@@ -1,7 +1,33 @@
 export default async (context) => {
     if (!(process.server)) {
+        // retrieve/set day from/to cookies
+        const __lastSavedCurrentDay = context.$cookies.get('CURRENT_DAY');
+        const __currentDayNow = new Date().toDateString();
+
+        if (__lastSavedCurrentDay) {
+            if (__lastSavedCurrentDay !== __currentDayNow) {
+                // not the same day anymore
+                // -> set today's date to vuex store
+                context.store.commit('moduleUser/setCurrentDay', { currentDay: __currentDayNow }, context); // todo
+
+                // & update cookies
+                context.$cookies.set('CURRENT_DAY', __currentDayNow);
+            } else {
+                // still the same day
+                // -> use saved date for vuex store
+                context.store.commit('moduleUser/setCurrentDay', { currentDay: __lastSavedCurrentDay }, context);
+            }
+        } else {
+            // no val yet saved to cookies todo: expire cookie?
+            context.$cookies.set('CURRENT_DAY', __currentDayNow);
+
+            context.store.commit('moduleUser/setCurrentDay', { currentDay: __currentDayNow }, context);
+        }
+
+
+        // retrieve data from localStorage
         await Promise.all([
-            await context.store.dispatch('moduleUser/retrieveSelectedTasksFromStorage', context),
+            await context.store.dispatch('moduleUser/retrieveSelectedTasksFromStorage', context), // this needs the current date for the UI
             await context.store.dispatch('moduleUser/retrieveBreaksFromStorage', context),
             await context.store.dispatch('moduleUser/retrieveBookmarksFromStorage', context),
             await context.store.dispatch('moduleUser/retrieveSelectionForSuggestionsFromStorage', context), // todo: as cookie?
