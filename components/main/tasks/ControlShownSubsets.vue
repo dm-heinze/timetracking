598010f90f43b2f1c22e83aa480643a14f48038e
dm-neslihan <genctuerk@digitalmanufaktur.com>
2020-12-27 19:20:53 +0100

    add template wrapper tags to visually group template parts for toggler & tab buttons

--- components/main/tasks/ControlShownSubsets.vue
+++ components/main/tasks/ControlShownSubsets.vue
@@ -1,41 +1,45 @@
 <template>
     <div class="toggle-show-all pb-2 d-flex flex-row justify-content-end align-items-center mr-4"> <!-- todo -->
         <!-- [ today / not-today ] Tabs --> <!-- todo -->
-        <button
-            @click="toggleUnbookedTasksNotOfTheDay"
-            class="mr-4 font-weight-bold"
-            :class="{ 'inactive': !showUnbookedTasksNotOfTheDay }"
-            :disabled="showUnbookedTasksNotOfTheDay"
-            v-if="unbookedTasksNotOfTheDay.length"
-        >
-            Previously
-        </button>
-        <button
-            @click="toggleUnbookedTasksNotOfTheDay"
-            class="mr-4 font-weight-bold"
-            :class="{ 'inactive': showUnbookedTasksNotOfTheDay }"
-            :disabled="!showUnbookedTasksNotOfTheDay"
-        >
-            Today
-        </button>
+        <template>
+            <button
+                @click="toggleUnbookedTasksNotOfTheDay"
+                class="mr-4 font-weight-bold"
+                :class="{ 'inactive': !showUnbookedTasksNotOfTheDay }"
+                :disabled="showUnbookedTasksNotOfTheDay"
+                v-if="unbookedTasksNotOfTheDay.length"
+            >
+                Previously
+            </button>
+            <button
+                @click="toggleUnbookedTasksNotOfTheDay"
+                class="mr-4 font-weight-bold"
+                :class="{ 'inactive': showUnbookedTasksNotOfTheDay }"
+                :disabled="!showUnbookedTasksNotOfTheDay"
+            >
+                Today
+            </button>
+        </template>
 
         <!-- [ unbooked only / booked + unbooked ] Toggler --> <!-- todo -->
-        <toggle-right-icon
-            class="toggle-show-all--show"
-            :class="{ 'disabled': showUnbookedTasksNotOfTheDay }"
-            v-b-tooltip.hover
-            title="Toggle Booked"
-            @click="toggleShowAllSelectedTasksOfCurrentDay()"
-            v-if="showAllSelectedTasksOfCurrentDay"
-        />
-        <toggle-left-icon
-            class="toggle-show-all--hide"
-            :class="{ 'disabled': showUnbookedTasksNotOfTheDay }"
-            v-b-tooltip.hover
-            title="Toggle Booked"
-            @click="toggleShowAllSelectedTasksOfCurrentDay()"
-            v-else
-        />
+        <template>
+            <toggle-right-icon
+                class="toggle-show-all--show"
+                :class="{ 'disabled': showUnbookedTasksNotOfTheDay }"
+                v-b-tooltip.hover
+                title="Toggle Booked"
+                @click="toggleShowAllSelectedTasksOfCurrentDay()"
+                v-if="showAllSelectedTasksOfCurrentDay"
+            />
+            <toggle-left-icon
+                class="toggle-show-all--hide"
+                :class="{ 'disabled': showUnbookedTasksNotOfTheDay }"
+                v-b-tooltip.hover
+                title="Toggle Booked"
+                @click="toggleShowAllSelectedTasksOfCurrentDay()"
+                v-else
+            />
+        </template>
     </div>
 </template>
 
